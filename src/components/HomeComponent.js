371f088621f9bc24722bc878490d9df267327a4e
drevlope <rezamuhammad23@gmail.com>
2020-09-15 16:31:55 +0700

    home unfinished

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -4,9 +4,10 @@ import { Container, Row, Col, Button, Image } from "react-bootstrap";
 export default class Dashboard extends  React.Component {
   render(){
     return (
+      <div>
       <Container>
         <Row className="section-one">
-          <Col className="sectionOneLeft" >
+          <Col xs={12} md={6} className="sectionOneLeft" >
             <div>
               <h1>BORDIR KOMPUTER CEPAT</h1>
               <p>
@@ -18,7 +19,6 @@ export default class Dashboard extends  React.Component {
               </Button>
             </div>
             <hr className="divider"></hr>
-
             <div class="icon-phone">
                 <div class="icon">
                     <span><i class='bx bxs-phone bx-border-circle bx-sm'></i></span>
@@ -30,30 +30,27 @@ export default class Dashboard extends  React.Component {
                     </div>
                 </div>
             </div>
-            <div>
-              <p>    
-                {/* <i class='bx bxs-phone bx-border-circle bx-sm'></i> */}
-              </p>
-            </div>
           </Col>
-          <Col className="sectionOneRight">
+          <Col xs={12} md={6} className="sectionOneRight hide-on-med">
             <div>
-              <Image src={require("../assets/images/section-one.png")} />
+              <Image className="img-responsive" src={require("../assets/images/section-one.png")} />
             </div>
           </Col>
         </Row>
+      </Container>
+      <Container>
         <Row className="section-two">
-        <Col>
+        <Col xs={12} md={6}>
           <div className="two-left">
-            <Image src={require("../assets/images/service.png")} />
+            <Image className="img-responsive" src={require("../assets/images/service.png")} />
           </div>
         </Col>
-        <Col>
+        <Col xs={12} md={6}>
             <div className="two-right">
               <h2>Lorem ipsum dolor sit amet</h2>
+              <h5>"Lorem ipsum dolor sit amet"</h5>
               <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p><p> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
               <Button className="btn-secondary" variant="primary">
                 Hubungi Kami
               </Button>
@@ -61,6 +58,36 @@ export default class Dashboard extends  React.Component {
           </Col>
         </Row>
       </Container>
+      <Container>
+        <Row className="section-three">   
+        <Col xs={12} md={6}>
+          <div className="three-left">
+            <h2>Lorem ipsum dolor sit amet</h2>
+            <h5>"Lorem ipsum dolor sit amet"</h5>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <Button className="btn-secondary" variant="primary">
+              Hubungi Kami
+            </Button>
+          </div>
+        </Col>
+        <Col xs={12} md={6}>
+          <div className="three-right">
+            <Image className="img-responsive" src={require("../assets/images/section-three.png")} />
+          </div>
+        </Col>
+        </Row>
+      </Container>
+      <div className="section-four">  
+        <Container>
+          <Row>   
+          <Col xs={12} md={6}>
+
+          </Col>
+          </Row>
+        </Container>
+      </div>
+      </div>
     );
   }
   
