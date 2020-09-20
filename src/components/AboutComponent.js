9b921fe0381d0e3a45316107c7834880da305d44
drevlope <rezamuhammad23@gmail.com>
2020-09-20 16:57:34 +0700

    home 4.0

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Container, Row, Col, Button, Image, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 export default class AboutPage extends  React.Component {
   render(){
@@ -36,50 +37,36 @@ export default class AboutPage extends  React.Component {
                 <h2 className="center">Aktivitas Terbaru Kami</h2>
               <Row>
                 <Col md={4} sm={12}>
-                  <Card style={{ width: '20rem' }}>
+                  <Card className="center" style={{ width: '20rem' }}>
                     <Card.Img variant="top" src={require("../assets/images/slide1.jpg")} />
                     <Card.Body>
+                      <Card.Text>20-09-2020</Card.Text>
                       <Card.Title>Card Title</Card.Title>
-                      <Card.Text>
-                        Some quick example text to build on the card title and make up the bulk of
-                        the card's content.
-                      </Card.Text>
+                        <Link>Selengkapnya</Link>
                     </Card.Body>
-                      <Card.Footer>
-                        <Button style={{ width: '100%' }}  className="btn-primary">Go somewhere</Button>
-                      </Card.Footer>
                   </Card>
                 </Col>
                 <Col md={4} sm={12}>
-                    <Card style={{ width: '20rem' }}>
-                      <Card.Img variant="top" src={require("../assets/images/slide1.jpg")} />
-                      <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
-                          Some quick example text to build on the card title and make up the bulk of
-                          the card's content.
-                        </Card.Text>
-                      </Card.Body>
-                      <Card.Footer>
-                        <Button style={{ width: '100%' }}  className="btn-primary">Go somewhere</Button>
-                      </Card.Footer>
+                  <Card className="center" style={{ width: '20rem' }}>
+                    <Card.Img variant="top" src={require("../assets/images/slide1.jpg")} />
+                    <Card.Body>
+                      <Card.Text>20-09-2020</Card.Text>
+                      <Card.Title>Card Title</Card.Title>
+                        <Link>Selengkapnya</Link>
+                    </Card.Body>
                   </Card>
                 </Col>
                 <Col md={4} sm={12}>
-                  <Card style={{ width: '20rem' }}>
+                  <Card className="center" style={{ width: '20rem' }}>
                     <Card.Img variant="top" src={require("../assets/images/slide1.jpg")} />
                     <Card.Body>
+                      <Card.Text>20-09-2020</Card.Text>
                       <Card.Title>Card Title</Card.Title>
-                      <Card.Text>
-                        Some quick example text to build on the card title and make up the bulk of
-                        the card's content.
-                      </Card.Text>
+                        <Link>Selengkapnya</Link>
                     </Card.Body>
-                    <Card.Footer>
-                      <Button style={{ width: '100%' }}  className="btn-primary">Go somewhere</Button>
-                    </Card.Footer>
                   </Card>
                 </Col>
+
               </Row>
             </Container>
         </div>
