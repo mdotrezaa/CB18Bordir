7e2732958693bea37ec5c973526a4c6be6c541da
drevlope <rezamuhammad23@gmail.com>
2020-10-01 16:14:58 +0700

    aktivitas edit 3.0

--- src/components/ActivityComponent.js
+++ src/components/ActivityComponent.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Row, Col, Button, Card, Tabs, Tab, Image } from "react-bootstrap";
+import { Container, Row, Col, Button, Card, Tabs, Tab, Image, Pagination } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 export default class ActivityPage extends  React.Component {
@@ -22,11 +22,38 @@ export default class ActivityPage extends  React.Component {
                 <div className="list-activity">
                     <Row>
                         <Col md={4}>
+                            <div className="thumbnail">
+                                <Image className="img-responsive" src={require("../assets/images/store.jpg")} />
+                            </div>
+                        </Col>
+                        <Col md={6}>
+                            <div className="list-header">
+                                <span>20/09/2020</span>
+                            </div>
+                            <div className="list-body">
+                                <h5><a href="#">Lorem Ipsum sit amet!</a></h5><br/>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                            </div>
+                            <div className="list-footer">
+                                <span>Author Name</span>
+                            </div>
+                        </Col>
+                        <Col className="read-left" md={2}>
+                            <Button className="read-link">Baca</Button>
+                        </Col>
+                    </Row>
+                </div>
+                <div className="divider"></div>
+                <div className="list-activity">
+                <Row>
+                        <Col md={4}>
+                            <div className="thumbnail">
                             <Image className="img-responsive" src={require("../assets/images/slide1.jpg")} />
+                            </div>
                         </Col>
-                        <Col md={8}>
+                        <Col md={6}>
                             <div className="list-header">
-                                <span>Date</span>
+                                <span>20/09/2020</span>
                             </div>
                             <div className="list-body">
                                 <h5><a href="#">Lorem Ipsum sit amet!</a></h5><br/>
@@ -35,18 +62,23 @@ export default class ActivityPage extends  React.Component {
                             <div className="list-footer">
                                 <span>Author Name</span>
                             </div>
+                        </Col>
+                        <Col className="read-left" md={2}>
+                            <Button className="read-link">Baca</Button>
                         </Col>
                     </Row>
                 </div>
                 <div className="divider"></div>
                 <div className="list-activity">
-                    <Row>
+                <Row>
                         <Col md={4}>
-                            <Image className="img-responsive" src={require("../assets/images/slide4.jpg")} />
+                            <div className="thumbnail">
+                                <Image className="img-responsive" src={require("../assets/images/store.jpg")} />
+                            </div>
                         </Col>
-                        <Col md={8}>
+                        <Col md={6}>
                             <div className="list-header">
-                                <span>Date</span>
+                                <span>20/09/2020</span>
                             </div>
                             <div className="list-body">
                                 <h5><a href="#">Lorem Ipsum sit amet!</a></h5><br/>
@@ -56,29 +88,24 @@ export default class ActivityPage extends  React.Component {
                                 <span>Author Name</span>
                             </div>
                         </Col>
-                    </Row>
-                </div>
-                <div className="divider"></div>
-                <div className="list-activity">
-                    <Row> 
-                        <Col md={4}>
-                            <Image className="img-responsive" src={require("../assets/images/slide2.jpg")} />
-                        </Col>
-                        <Col md={8}>
-                            <div className="list-header">
-                                <span>Date</span>
-                            </div>
-                            <div className="list-body">
-                                <h5><a href="#">Lorem Ipsum sit amet!</a></h5><br/>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                            </div>
-                            <div className="list-footer">
-                                <span>Author Name</span>
-                            </div>
+                        <Col className="read-left" md={2}>
+                            <Button className="read-link">Baca</Button>
                         </Col>
                     </Row>
                 </div> 
             </Container>
+        </div>
+        <div className="text-center">
+        <Pagination>
+            <Pagination.First />
+            <Pagination.Prev />
+            <Pagination.Item>{1}</Pagination.Item>
+            <Pagination.Item>{2}</Pagination.Item>
+            <Pagination.Item>{3}</Pagination.Item>
+            <Pagination.Ellipsis />
+            <Pagination.Next />
+            <Pagination.Last />
+        </Pagination>
         </div>
       </div>
     );
